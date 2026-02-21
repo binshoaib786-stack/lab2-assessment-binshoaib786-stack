e13c62fe921204ccfc7bfd9a411f220273151b71
binshoaib786-stack <binshoaib786@gmail.com>
2026-02-21 17:15:46 +0500

    Add functions to return float and char values

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -8,18 +8,23 @@ int getIntValue() {
 }
 
 float getFloatValue() {
+    float var_float = 10.75;
+    return var_float;
     // Declare a variable var_float of type float and set its value to 10.75.
     //return var_float;
 }
 
 char getCharValue() {
+    char var_char = 'A';
+    return var_char;
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
 }
 
 int convertFloatToInt(float value) {
+    
     // convert value to int.
-    return value;
+    return (int)value;
 }
 
 /*
@@ -29,6 +34,9 @@ Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
 float fahrenheitToCelsius(float fahrenheit) {
+    float Celsius;
+    Celsius = (fahrenheit - 32) * 5.0 / 9.0;
+    return Celsius;
     //return celsius;
 
 }
